b40b5e2c4dbf06897d99872bf83d01a2e7ca513a
Vincent-lebrun <lebrunvincent12@gmail.com>
2022-05-26 10:28:26 +0200

    Add User repository to call function  with getAll

--- src/main/java/com/vincent/gestiondestock/repository/UserRepository.java
+++ src/main/java/com/vincent/gestiondestock/repository/UserRepository.java
@@ -1,2 +1,7 @@
-package com.vincent.gestiondestock.repository;public interface UserRepository {
+package com.vincent.gestiondestock.repository;
+
+import com.vincent.gestiondestock.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User , Long> {
 }
