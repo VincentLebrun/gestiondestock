64b777954bbe929444d2f092811a848a4a563063
Vincent-lebrun <lebrunvincent12@gmail.com>
2022-05-26 10:29:05 +0200

    Add User service to create function  getAll

--- src/main/java/com/vincent/gestiondestock/service/UserService.java
+++ src/main/java/com/vincent/gestiondestock/service/UserService.java
@@ -1,2 +1,18 @@
-package com.vincent.gestiondestock.service;public class UserService {
+package com.vincent.gestiondestock.service;
+
+import com.vincent.gestiondestock.model.User;
+import com.vincent.gestiondestock.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
+
 }
