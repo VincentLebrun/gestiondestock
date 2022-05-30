6fb1f50d079c28a8c24e91b4dd7c16ca6e387331
Vincent-lebrun <lebrunvincent12@gmail.com>
2022-05-30 16:53:09 +0200

    switch method of authorization

--- src/main/java/com/vincent/gestiondestock/controller/MySecurityConfig.java
+++ src/main/java/com/vincent/gestiondestock/controller/MySecurityConfig.java
@@ -1,2 +1,23 @@
-package com.vincent.gestiondestock.controller;public class MySecurityConfig {
+package com.vincent.gestiondestock.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+              .authorizeRequests()
+              .antMatchers("dashboard").authenticated()
+              .antMatchers("/home").permitAll()
+              .and()
+              .formLogin()
+              .hashCode()
+      //Hashcode à la place de httpBasic() ??
+              ;
+    }
 }
